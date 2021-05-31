69fae6259ffc536100f7a5df8362cf5dc597f5c8
dbklor <wuchwusul@gmail.com>
2021-05-31 14:29:49 +0200

    removing foncusing document

--- Subgroup1/code/ReadExchangeFileExample_WKRDBEST2.R
+++ Subgroup1/code/ReadExchangeFileExample_WKRDBEST2.R
@@ -11,19 +11,16 @@ options(scipen=500) # big number of digits
 
 # Load the validation data
 #validationData <- getValidationData(downloadFromGitHub = FALSE, fileLocation = './tableDefs/BaseTypes.xsd')
-validationData <- getValidationData(downloadFromGitHub = TRUE, fileLocation = './tableDefs/BaseTypes.xsd')
-
-# 11/9/2020 Temp fix because the validation fields aren't up to date :-(
-validationData[validationData$type == 'tRS_Stratification','type'] <- 'tYesNoFields'
+validationData <- getValidationData(downloadFromGitHub = FALSE, fileLocation = './tableDefs/BaseTypes.xsd')
 
 # Load the reference data: either refresh from ICES or just use a local copy
 #allowedValues <- loadReferenceData(downloadFromICES = FALSE)
-allowedValues <- loadReferenceData(downloadFromICES = TRUE, validationData=validationData)
+allowedValues <- loadReferenceData(downloadFromICES = FALSE, validationData=validationData)
 
 # Load the lists of tables required for each hierarchy: either refresh from ICES or just use a local copy
 #allRequiredTables <- getTablesInHierarchies(downloadFromGitHub = FALSE, fileLocation = './tableDefs/')
-allRequiredTables <- getTablesInHierarchies(downloadFromGitHub = TRUE, fileLocation = './tableDefs/')
+allRequiredTables <- getTablesInHierarchies(downloadFromGitHub = FALSE, fileLocation = './tableDefs/')
 
-myExchangeFileVD <- readExchangeFile(RDBESvalidationdata = validationData, nameOfFile = 'output/uploaded/DK_1966_HVD.csv' )
+myExchangeFileVD <- readExchangeFile(RDBESvalidationdata = validationData, nameOfFile = '../inputs/DK_1966_HVD.csv' )
 myExchangeFileSL <- readExchangeFile(RDBESvalidationdata = validationData, nameOfFile = 'output/uploaded/DK_1966_HSL.csv' )
-myExchangeFileH1 <- readExchangeFile(RDBESvalidationdata = validationData, nameOfFile = 'output/uploaded/DK_1966_H1.csv',RequiredTables = allRequiredTables )
+myExchangeFileH1 <- readExchangeFile(RDBESvalidationdata = validationData, nameOfFile = '../inputs/DK_1966_H1.csv',RequiredTables = allRequiredTables )
