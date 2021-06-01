cb22ecfaccd8b0bdca7dac2b35d14cda2d140712
edvinf <edvin.fuglebakk@gmail.com>
2021-06-01 11:45:32 +0200

    init

---
+++ Subgroup2/personal/Edvin/ratioEstTest/R/samples_datadoc.R
@@ -0,0 +1,18 @@
+#' Example data - Norwegian catch of mac.27.nea sampled in 2019 in RDBES format
+#' Data was obtained from RDBES format as of the 2020 test data called.
+#' The exchange format was parsed with the parser at:
+#' https://github.com/davidcurrie2001/MI_RDBES_ExchangeFiles/blob/d38b75fe6a902892943cf01100cdbb7e9b13c21a/ReadExchangeFileExample_WKRDBEST2.R
+#'
+#'
+#' @docType data
+#'
+#' @usage data(samples)
+#'
+#' @format list of 8 data frames. Variables documented in https://github.com/ices-tools-dev/RDBES
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(samples)
+#'
+"samples"
